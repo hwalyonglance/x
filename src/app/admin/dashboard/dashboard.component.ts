b43bbc82f40103eb3fcc79c9c6630afc578fb8e3
Rusman Wahab <hwalyonglance>
2019-05-31 21:57:16 +0700

    wip

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -11,78 +11,63 @@ import { map } from 'rxjs/operators';
 export class DashboardComponent implements OnInit {
 	cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
 		map(({ matches }) => {
-			if (matches) {
-				return [
-					{ title: 'Card 1', cols: 1, rows: 2 },
-					{ title: 'Card 2', cols: 1, rows: 1 },
-					{ title: 'Card 3', cols: 1, rows: 1 },
-					{ title: 'Card 4', cols: 2, rows: 2 },
-					{ title: 'Card 5', cols: 1, rows: 1 },
-					{ title: 'Card 6', cols: 1, rows: 2 },
-					{ title: 'Card 7', cols: 2, rows: 1 },
-					{ title: 'Card 8', cols: 2, rows: 1 },
-					{ title: 'Card 9', cols: 1, rows: 1 },
-					{ title: 'Card 10', cols: 1, rows: 1 },
-				];
-			}
-
 			return [
 				{
-					title: 'desktop 1', cols: 2, rows: 1,
+					title: 'desktop 1', img: {height: 150,width:350},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.`,
 				},
 				{
-					title: 'desktop 2', cols: 1, rows: 1,
+					title: 'desktop 2', img: {height: 250,width:350},
 					text: `Lorem ipsum dolor sit amet.`,
 				},
 				{
-					title: 'desktop 3', cols: 1, rows: 2,
+					title: 'desktop 3', img: {height: 450,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua.`,
 				},
 				{
-					title: 'desktop 4', cols: 1, rows: 1,
+					title: 'desktop 4', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua.`,
 				},
 				{
-					title: 'desktop 5', cols: 1, rows: 1,
+					title: 'desktop 5', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore.`,
 				},
 				{
-					title: 'desktop 6', cols: 1, rows: 2,
+					title: 'desktop 6', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.`,
 				},
 				{
-					title: 'desktop 7', cols: 1, rows: 2,
+					title: 'desktop 7', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.`,
 				},
 				{
-					title: 'desktop 8', cols: 1, rows: 1,
+					title: 'desktop 8', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.`,
 				},
 				{
-					title: 'desktop 9', cols: 1, rows: 1,
+					title: 'desktop 9', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 				consequat.`,
 				},
 				{
-					title: 'desktop 10', cols: 1, rows: 1,
+					title: 'desktop 10', img: {height: 150,width:250},
 					text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
